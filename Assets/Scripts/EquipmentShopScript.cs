8164af5a6ae7cfc2050f8cf3ff3b9c7a6e981661
Hayden <whitefiregames1@hotmail.com>
2022-02-24 15:00:04 +1100

    Started work on equipment shop

---
+++ Assets/Scripts/EquipmentShopScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentShopScript : MonoBehaviour
+{
+    #region Singleton
+    public static EquipmentShopScript instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of EquipmentShopScript found");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public GameObject shopPanel;
+
+    public void ShowShopPanel()
+    {
+        shopPanel.SetActive(true);
+    }
+}
